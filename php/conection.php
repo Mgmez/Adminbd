cb892568f0153934920af7d55cf585c1dba28645
Jose Aguayo <jmiguel.aguayo@alumnos.udg.mx>
2018-11-28 04:14:52 -0600

    backend

--- php/conection.php
+++ php/conection.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "foranerp";
-$pass = "12345"
-$db = "users"
+$user = "foranerp_user";
+$pass = "m?L$GPQ5!r[Y"
+$db = "foranerp_MAE"
 $connection = mysqli_connect($host,$user,$pass,$db);
 mysqli_set_charset($connection, "utf8");
 ?>
--- php/insert.php
+++ php/insert.php
@@ -1,8 +1,13 @@
 <?
-$user = $_POST["uname"];
-$pass =  $_POST["pass"];
+$code = $_POST["user_code"];
+$name =  $_POST["user_name"];
+$surname =  $_POST["user_surname"];
+$email =  $_POST["user_email"];
+$pass =  $_POST["user_password"];
+$job =  $_POST["user_job"];
+$account =  $_POST["account"];
 require("connection.php");
-$sql = "INSERT INTO users ('',username, pass) VALUES ('$user', '$pass')";
+$sql = "INSERT INTO users (id,code,name,surnames,email,pass,career,user) VALUES (' ','$code', '$name', '$surname', '$email', '$pass', '$job', '$account')";
 mysqli_query($connection,$sql);
 echo 1;
 ?>
